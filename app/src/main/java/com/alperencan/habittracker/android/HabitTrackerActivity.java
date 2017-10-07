34a1f61ec7e136b8096cd6bff83e2f49aebbe2a3
Alp Eren Can <alperenc@gmail.com>
2017-10-07 16:05:56 +0300

    Initial commit

---
+++ app/src/main/java/com/alperencan/habittracker/android/HabitTrackerActivity.java
@@ -0,0 +1,13 @@
+package com.alperencan.habittracker.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabitTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_tracker);
+    }
+}
